a6ef650c9551c2d041dd265d023d713952ea2bb9
jonwin1 <jonatan.wincent@proton.me>
2024-03-15 22:42:50 +0100

    remaps

--- lua/jonwin/remap.lua
+++ lua/jonwin/remap.lua
@@ -1,38 +1,46 @@
 vim.g.mapleader = " "
 vim.g.maplocalleader = " "
 
--- [[ Basic Keymaps ]]
---  See `:help vim.keymap.set()`
-
--- Set highlight on search, but clear on pressing <Esc> in normal mode
-vim.opt.hlsearch = true
-vim.keymap.set("n", "<Esc>", "<cmd>nohlsearch<CR>")
-
 -- Diagnostic keymaps
 vim.keymap.set("n", "[d", vim.diagnostic.goto_prev, { desc = "Go to previous [D]iagnostic message" })
 vim.keymap.set("n", "]d", vim.diagnostic.goto_next, { desc = "Go to next [D]iagnostic message" })
 vim.keymap.set("n", "<leader>e", vim.diagnostic.open_float, { desc = "Show diagnostic [E]rror messages" })
 vim.keymap.set("n", "<leader>q", vim.diagnostic.setloclist, { desc = "Open diagnostic [Q]uickfix list" })
 
--- Exit terminal mode in the builtin terminal with a shortcut that is a bit easier
--- for people to discover. Otherwise, you normally need to press <C-\><C-n>, which
--- is not what someone will guess without a bit more experience.
---
--- NOTE: This won't work in all terminal emulators/tmux/etc. Try your own mapping
--- or just use <C-\><C-n> to exit terminal mode
-vim.keymap.set("t", "<Esc><Esc>", "<C-\\><C-n>", { desc = "Exit terminal mode" })
-
 -- TIP: Disable arrow keys in normal mode
--- vim.keymap.set('n', '<left>', '<cmd>echo "Use h to move!!"<CR>')
--- vim.keymap.set('n', '<right>', '<cmd>echo "Use l to move!!"<CR>')
--- vim.keymap.set('n', '<up>', '<cmd>echo "Use k to move!!"<CR>')
--- vim.keymap.set('n', '<down>', '<cmd>echo "Use j to move!!"<CR>')
+vim.keymap.set("n", "<left>", '<cmd>echo "Use h to move!!"<CR>', { desc = "which_key_ignore" })
+vim.keymap.set("n", "<right>", '<cmd>echo "Use l to move!!"<CR>', { desc = "which_key_ignore" })
+vim.keymap.set("n", "<up>", '<cmd>echo "Use k to move!!"<CR>', { desc = "which_key_ignore" })
+vim.keymap.set("n", "<down>", '<cmd>echo "Use j to move!!"<CR>', { desc = "which_key_ignore" })
 
--- Keybinds to make split navigation easier.
---  Use CTRL+<hjkl> to switch between windows
---
---  See `:help wincmd` for a list of all window commands
-vim.keymap.set("n", "<C-h>", "<C-w><C-h>", { desc = "Move focus to the left window" })
-vim.keymap.set("n", "<C-l>", "<C-w><C-l>", { desc = "Move focus to the right window" })
-vim.keymap.set("n", "<C-j>", "<C-w><C-j>", { desc = "Move focus to the lower window" })
-vim.keymap.set("n", "<C-k>", "<C-w><C-k>", { desc = "Move focus to the upper window" })
+vim.keymap.set("n", "<leader>pv", vim.cmd.Ex, { desc = "netrw" })
+
+vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv", { desc = "Move selection [J]" })
+vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv", { desc = "Move selection [K]" })
+
+vim.keymap.set("n", "<C-d>", "<C-d>zz", { desc = "which_key_ignore" })
+vim.keymap.set("n", "<C-u>", "<C-u>zz", { desc = "which_key_ignore" })
+vim.keymap.set("n", "n", "nzzzv", { desc = "which_key_ignore" })
+vim.keymap.set("n", "N", "Nzzzv", { desc = "which_key_ignore" })
+
+vim.keymap.set("x", "<leader>p", '"_dP', { desc = "[P]aste without copy" })
+
+--vim.keymap.set("n", "<leader>y", '"+y')
+--vim.keymap.set("v", "<leader>y", '"+y')
+--vim.keymap.set("n", "<leader>Y", '"+Y')
+
+--vim.keymap.set("n", "<leader>d", '"_d')
+--vim.keymap.set("v", "<leader>d", '"_d')
+
+vim.keymap.set("n", "Q", "<nop>", { desc = "which_key_ignore" })
+vim.keymap.set("n", "<leader>f", function()
+	vim.lsp.buf.format()
+end, { desc = "[F]ormat" })
+
+vim.keymap.set("n", "<C-k>", "<cmd>cnext<CR>zz", { desc = "which_key_ignore" })
+vim.keymap.set("n", "<C-j>", "<cmd>cprev<CR>zz", { desc = "which_key_ignore" })
+vim.keymap.set("n", "<leader>k", "<cmd>lnext<CR>zz", { desc = "which_key_ignore" })
+vim.keymap.set("n", "<leader>j", "<cmd>lprev<CR>zz", { desc = "which_key_ignore" })
+
+--vim.keymap.set("n", "<leader>s", ":%s/\\<<C-r><C-w>\\>/<C-r><C-w>/gI<Left><Left><Left>")
+--vim.keymap.set("n", "<leader>x", "<cmd>!chmod +x %<CR>", { silent = true })
